9c98cd7f7f678bd651b4168885f3a3cd9b98f7fe
ltdog <2397476604@qq.com>
2024-10-10 19:41:09 +0800

    24

--- vue-rabbit/vite.config.js
+++ vue-rabbit/vite.config.js
@@ -2,15 +2,38 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 1.配置elementPlus采用scss样式配色系统
+        ElementPlusResolver({importStyle: "sass"})
+    ],
+    }),
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+        scss: {
+            // 2.自动导入定制化样式文件进行样式覆盖
+            additionalData: `
+                  @use "@/styles/element/index.scss" as *;       
+    `,
+        }
+    }
+}
 })
