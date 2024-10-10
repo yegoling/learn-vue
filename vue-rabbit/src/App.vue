0ce073d17ba7c5ff70fdfdc4c9b0baa4d520a9a8
ltdog <2397476604@qq.com>
2024-10-10 23:16:12 +0800

    28

--- vue-rabbit/src/App.vue
+++ vue-rabbit/src/App.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <RouterView></RouterView>
+  <RouterView>
+    <div class="test">test</div>
+  </RouterView>
 </template>
+
+<style scoped lang="scss">
+.test {
+  color: $sucColor;
+}
+</style>
